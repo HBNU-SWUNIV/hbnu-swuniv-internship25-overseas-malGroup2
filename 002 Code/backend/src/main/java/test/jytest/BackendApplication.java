99a5bf5495ffd8ac1eae3a89296bbebd535306fd
junseong00 <js03093351@gmail.com>
2025-08-29 16:39:27 +0900

    upload backend files

---
+++ 002 Code/backend/src/main/java/test/jytest/BackendApplication.java
@@ -0,0 +1,17 @@
+package test.jytest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // test.jytest 및 하위 패키지 전부 스캔
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		try{
+			SpringApplication.run(BackendApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
